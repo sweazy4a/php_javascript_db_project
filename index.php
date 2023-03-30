c9ac196bd50e0b040ba6f28d89bbc50ba6b40320
Vladyslav Radchenko <vladcommunication@gmail.com>
2023-03-30 08:51:20 +0200

    refactor index.php

---
+++ index.php
@@ -0,0 +1,7 @@
+<?php 
+    /* Vladyslav Radchenko -> vladcommunication@gmail.com */
+    include('source/views/header-view.php');
+    include('source/views/form-view.php');
+    include('source/views/footer-view.php');
+ ?>
+    
